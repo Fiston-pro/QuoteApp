ecd919897e677dd4ccf0ce633a8876031f01cc8d
Fiston-pro <byiringiroetienne2@gmail.com>
2021-10-28 20:32:59 +0200

    Added a background picture and redesigned the look of a home screen

--- lib/home.dart
+++ lib/home.dart
@@ -13,31 +13,36 @@ class _homeState extends State<home> {
 
   List<Quote> quotes = [
     Quote('Legend have fiston will finish the app','Fiston the gr8'),
-    Quote('Thanks my son','Mom'),
-    Quote('Welcome to our company', 'FAANG interviewer')
+    Quote('Stay Hungry, Stay Foolish','Steve Jobs'),
+    Quote('An eye for an eye, makes the whole world blind', 'Ghandhi')
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar:true ,
       appBar: AppBar(
-        backgroundColor: Colors.grey,
-        title: Center(child: Text('Quote App', style: TextStyle(
-            color: Colors.white,fontSize: 25),),),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text("My Quotes",style: TextStyle(fontWeight: FontWeight.w600,
+                fontSize: 23, color: Colors.white),),
       ),
       body: Container(
-        constraints: BoxConstraints.expand(),
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("images/day.jpg"),
-                        fit: BoxFit.cover)),
-        child: Column(
-          children: quotes.map((quote) => QuoteCard(quote: quote,delete: (){
-            setState(() {
-              quotes.remove(quote);
-            });
-          } )
-        ).toList()
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/steve_jobs.jpeg"),
+                fit: BoxFit.cover)),
+        child: SafeArea(
+          child: Column(
+            children: 
+            quotes.map((quote) => QuoteCard(quote: quote,delete: (){
+              setState(() {
+                quotes.remove(quote);
+              });
+            } )
+          ).toList()
+          ),
         ),
       )
       );
--- lib/quoteCard.dart
+++ lib/quoteCard.dart
@@ -13,17 +13,24 @@ class QuoteCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0) ,
+      color: Colors.transparent,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text('Quote: ${quote.quote}', style: TextStyle(fontWeight: FontWeight.w600,
-                fontSize: 16, color: Colors.black87)),
-            Text('Author: ${quote.author}', style: TextStyle(fontWeight: FontWeight.w300,
-            fontSize: 13)),
-            IconButton( icon: Icon(Icons.delete),onPressed:delete)
+            Text('${quote.quote}', style: TextStyle(fontWeight: FontWeight.w600,
+                fontSize: 16, color: Colors.white)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(height: 25,),
+                Text('~ ${quote.author}', style: TextStyle(fontWeight: FontWeight.w300,
+                fontSize: 13,color: Colors.white)),
+                IconButton( icon: Icon(Icons.delete),onPressed:delete, color: Colors.white,)
+
+              ],
+            ),
           ],
         ),
       ),
