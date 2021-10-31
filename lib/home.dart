b9fb7e47d53a602aaa55d5c68c7de94eff18a9d6
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2021-10-31 10:23:44 +0100

    Added the button to pop a dialog box for adding a quote

--- lib/home.dart
+++ lib/home.dart
@@ -11,7 +11,14 @@ class home extends StatefulWidget {
 
 class _homeState extends State<home> {
 
+  // Stuff for the database
   QuoteProvider dbService = QuoteProvider();
+
+  // Stuff for the dialog
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _textEditingAuthor = TextEditingController();
+  final TextEditingController _textEditingQuote = TextEditingController();
+
 
   List<Quote> quotes = [
     Quote('Legend have fiston will finish the app','Fiston the gr8'),
@@ -57,10 +64,92 @@ class _homeState extends State<home> {
           ).toList()
           ),
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+                await showInformationDialog(context);
+              },
+        child: Icon(Icons.add,),
+        backgroundColor: Colors.black,
+      ),
       );
 
   }
+
+  // function for the dialog
+  Future<void> showInformationDialog(BuildContext context) async {
+    return await showDialog(
+        context: context,
+        builder: (context) {
+          bool isChecked = false;
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              backgroundColor: Colors.black87,
+              content: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        controller: _textEditingAuthor,
+                        validator: (value) {
+                          return value!.isNotEmpty ? null : "Enter an author";
+                        },
+                        maxLength: 20,
+                        decoration:
+                            InputDecoration(
+                              hintText: "Author",
+                              hintStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.w500,letterSpacing: 1),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                              ),
+                            
+                      ),
+                      TextFormField(
+                        controller: _textEditingQuote,
+                        validator: (value) {
+                          return value!.isNotEmpty ? null : "Enter a Quote";
+                        },
+                        maxLength: 500,
+                        maxLines: 5,
+                        cursorColor: Colors.black,
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "Quote",
+                          hintStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.w500,letterSpacing: 1),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20)
+                          ),
+                
+                      ),)
+                    ],
+                  )),
+              title: Text('New Quote', style: TextStyle(color: Colors.white),),
+              actions: <Widget>[
+                InkWell(
+                  child: Text('OK   ',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,letterSpacing: 1),),
+                  onTap: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Do something like updating SharedPreferences or User Settings etc.
+                      Navigator.of(context).pop();
+                    }
+                  },
+                ),
+              ],
+            );
+          });
+        });
+  }
+
 }
 
 
